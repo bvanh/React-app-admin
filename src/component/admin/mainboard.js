512b7a9e2534a4c6ad767c804502ad3aa909bc70
bvanh <48560384+bvanh@users.noreply.github.com>
2019-10-16 07:36:09 +0700

    chang item

--- src/component/admin/mainboard.js
+++ src/component/admin/mainboard.js
@@ -42,14 +42,13 @@ export default function Mainboard(props) {
                 <Header style={{ background: '#fff', padding: 0 }} />
                 <Content style={{ margin: '0 16px' }}>
                     <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>List</Breadcrumb.Item>
-                        <Breadcrumb.Item>Detail</Breadcrumb.Item>
+                        <Breadcrumb.Item>Danh sách hotel</Breadcrumb.Item>
                     </Breadcrumb>
                     <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
                         <Danhsach />
                     </div>
                 </Content>
-                <Footer style={{ textAlign: 'center' }}>this is futter</Footer>
+                <Footer style={{ textAlign: 'center' }}>this is Futter</Footer>
             </Layout>
         </Layout>
     )
