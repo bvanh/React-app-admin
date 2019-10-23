d7eb1118cc0d4b32cc1078238b76377d98f1089c
bvanh <48560384+bvanh@users.noreply.github.com>
2019-10-23 22:43:33 +0700

    updates

--- src/component/admin/mainboard.js
+++ src/component/admin/mainboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb, Icon, Button, Upload, Input } from 'antd';
+import { Layout, Menu, Breadcrumb, Icon, Button, Upload, Input, Form } from 'antd';
 import Danhsach from '../admin/table';
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
@@ -22,7 +22,7 @@ export default function Mainboard(props) {
             thumbUrl: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
         },
     ];
-    
+
     const demo = {
         action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
         listType: 'picture',
@@ -67,20 +67,26 @@ export default function Mainboard(props) {
                                 <Breadcrumb.Item>Thêm mới</Breadcrumb.Item>
                             </Breadcrumb>
                             <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                                Tên <Input placeholder="Nhập tên hotel" /><br />
-                                Vị trí <Input placeholder="Nhập vị trí hotel" /><br />
-                                Đánh giá <Input placeholder="Nhập đánh giá hotel" /><br />
-                                Giá <Input placeholder="Nhập giá hotel" /><br />
-                                <div>
-                                    Thêm ảnh<br/>
-                                    <Upload {...demo}>
-                                        <Button>
-                                            <Icon type="upload" /> Upload
-      </Button>
-                                    </Upload>
-                                    <br />
-                                </div>,
-                        </div>
+                                <Form>
+                                    Tên <Input placeholder="Nhập tên hotel" required /><br />
+                                    Vị trí <Input placeholder="Nhập vị trí hotel" required /><br />
+                                    Đánh giá <Input placeholder="Nhập đánh giá hotel" required /><br />
+                                    Giá <Input placeholder="Nhập giá hotel" required /><br />
+                                    <div>
+                                        Thêm ảnh<br />
+                                        <Upload {...demo}>
+                                            <Button>
+                                                <Icon type="upload" /> Upload
+                                        </Button>
+                                        </Upload>
+                                        <br />
+                                    </div>
+                                    <Button 
+                                    type="primary"
+                                    htmlType="submit"
+                                    >Submit</Button>
+                                </Form>
+                            </div>
                         </Content>
                     )
                     :
